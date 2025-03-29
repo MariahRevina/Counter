c0b63cd48636d1e86d44b07116883c4efc3172c5
Mariah Revina <mashka_mal@mail.ru>
2025-03-29 22:04:35 +0300

    Добавлена кнопка и лэйбл. Созданы связи. Написан код.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterValue: UILabel!
+    
+    private var push = 0
+    
+    @IBAction func button(_ sender: Any) {
+        push += 1
+        counterValue.text = "Значение счётчика: \(push)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
